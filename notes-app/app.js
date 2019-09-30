7148574ef0adc13bcfd36652dc8dfb5fe9a0eb68
AmaldevAD <amaldev.ad71868@github.com>
2019-09-30 09:55:17 +0530

    2nd comit

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,12 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-fs.writeFileSync('notes.txt','Amal was created by js')
-fs.appendFile('notes.txt','Appended file')
+// fs.writeFileSync('notes.txt','Amal was created by js')
+// fs.appendFile('notes.txt','Appended file')
+
+
+const add=require('./util')
+
+const sum=add(5,8)
+console.log(sum)
+
+
---
+++ notes-app/util.js
@@ -0,0 +1,8 @@
+console.log('util.js')
+
+
+const add = function(a,b){
+    return a+b;
+
+}
+module.exports=add
