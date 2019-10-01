81aa7694d5d3640a68bd0e55fbf312922091a96e
AmaldevAD <amaldev.ad71868@github.com>
2019-10-01 16:50:46 +0530

    json parsing and stringyfy

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,22 @@
+const fs=require('fs')
+
 const book ={
     title:"Your Dreams are mine now",
     author:"Ravinder Singh"
 }
 
 const bookJSON=JSON.stringify(book)
-console.log(bookJSON)
+// console.log(bookJSON)
+//fs.writeFileSync('1-json.json',bookJSON)
+// const parseData=JSON.parse(bookJSON)
+// console.log(parseData.author)
+const data = fs.readFileSync('1-json.json').toString()
+
+const pData=JSON.parse(data)
+pData.name="AMAl"
+pData.age="19"
+
+const writeData=JSON.stringify(pData)
+
+fs.writeFileSync('1-json.json',writeData)
+console.log(pData)
